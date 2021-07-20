091941a89f117aff045e70448db94286f11aa04e
dami <dlola1718@gmail.com>
2021-07-20 10:13:41 +0100

    Sentiment Analysis

---
+++ PY_SentimentAnalysis/twitterSentiment.py
@@ -0,0 +1,4 @@
+#This is a sentiment analysis tool to test the polarity of stocks / tweets
+#Future update will include feelings and emotions (angry, happy, sad, etc), urgency (urgent, not urgent) and even intentions (interested v. not interested).
+
+
