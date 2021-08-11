c9f0f366341500c38f798ba9cc39f56b7570a86b
dami <dlola1718@gmail.com>
2021-08-11 22:21:39 +0100

    Pause TwitterSentiment

--- PY_SentimentAnalysis/twitterSentiment.py
+++ PY_SentimentAnalysis/twitterSentiment.py
@@ -2,3 +2,22 @@
 #Future update will include feelings and emotions (angry, happy, sad, etc), urgency (urgent, not urgent) and even intentions (interested v. not interested).
 
 
+# required libraries
+import tweepy #An easy-to-use Python library for accessing the Twitter API.
+from textblob import WordCloud #library for processing textual data
+import pandas as pd #This tool will be quite handy for exploring text data and making reports more lively
+import numpy as np
+import re #regular expression
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight') #plt lib styling
+
+
+#load data
+#Authentication details required from https://developer.twitter.com/en/apps
+
+
+
+#paused paused paused paused
+
+
+
